5b5498627729cb7dbfb57c8cbb98dc55a5ee001d
Jonathan King <jonathan.j.king@gmail.com>
2018-04-03 20:43:29 +0100

    conditional join

--- main.py
+++ main.py
@@ -67,7 +67,12 @@ async def on_message(message):
 async def play_sound_for_message(message, sound):
     author = message.author
     voice_channel = author.voice_channel
-    vc = await client.join_voice_channel(voice_channel)
+    bot_voice = client.user.voice
+    if bot_voice:
+        if bot_voice.voice_channel != voice_channel:
+            vc = await client.join_voice_channel(voice_channel)
+        else:
+            vc = bot_voice.voice_channel
     audio_path = "audio/{}.mp3".format(sound)
     player = vc.create_ffmpeg_player(audio_path)
     player.start()
